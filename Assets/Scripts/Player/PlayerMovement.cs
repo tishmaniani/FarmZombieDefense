0834b5270898cde2953c8f9a4ec34df2cade4728
Serho <supertishman@yandex.ru>
2026-03-03 18:28:50 +0300

    player movement getaxisraw

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,15 +2,35 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+
+    Rigidbody rb;
+    private Vector3 moveDirection;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX 
+                       | RigidbodyConstraints.FreezeRotationZ;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float verctical = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector3(horizontal, 0f, verctical).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+
+        if(moveDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, 10f * Time.fixedDeltaTime);
+        }
     }
 }
